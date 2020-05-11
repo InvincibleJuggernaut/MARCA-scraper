e5bb44028ff7de93f9d3f76e42af1dd67b330f12
Manikandan Nair <mani4pmn@gmail.com>
2020-05-11 12:04:42 +0530

    Create web_scraper.py

---
+++ web_scraper.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+url='https://www.marca.com/en/football/real-madrid.html'
+r=requests.get(url)
+main_page_news=r.content
+soup=BeautifulSoup(main_page_news,'html.parser')
+main_page_headlines=soup.find_all('h3', 'mod-title')
+len(main_page_headlines)
+
+#Link of the articles
+queries=4
+list_of_links=[]
+list_of_titles=[]
+complete_para=[]
+for i in range(0,queries):
+    link_url=main_page_headlines[i].find('a')['href']
+    list_of_links.append(link_url)
+    
+#Title of the articles
+    title=main_page_headlines[i].find('a')['title']
+    list_of_titles.append(title)
+    
+#Content of the headlines
+    real_story=requests.get(link_url)
+    story=real_story.content
+    soup_content=BeautifulSoup(story,'html.parser')
+    main_story=soup_content.find_all('p')
+    for j in range(0,len(main_story)):
+        para=main_story[j].get_text()
+        complete_para.append(para)
+        final_para=" ".join(complete_para)
+
+final_para=final_para.replace('Editions: En/football/real-madrid','')
+final_para=final_para.replace('Unidad Editorial Información Deportiva, S.L.U. Todos los derechos reservados. Follow us Editions: En/football/real-madrid','')
+final_para=final_para.replace(' Unidad Editorial Información Deportiva, S.L.U. Todos los derechos reservados. Follow us ','')
+final_para=final_para.replace("""© Mayo
+2020"""," ")
+print(final_para)
+
+
+
+
+
+
+
