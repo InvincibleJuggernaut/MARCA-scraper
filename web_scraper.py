f1fe34004a5fd63b0e77d8c64d150a45962b79f3
Manikandan Nair <mani4pmn@gmail.com>
2020-06-12 12:04:11 +0530

    Fix for June

--- web_scraper.py
+++ web_scraper.py
@@ -37,6 +37,8 @@ for i in range(0,queries):
     complete_para_final=complete_para_final.replace(' Unidad Editorial Información Deportiva, S.L.U. Todos los derechos reservados. Follow us','')
     complete_para_final=complete_para_final.replace("""© Mayo
 2020"""," ")
+    complete_para_final=complete_para_final.replace("""© Junio
+2020""","")
     complete_para_final_list.append(complete_para_final)
     
     print(list_of_titles[i])
